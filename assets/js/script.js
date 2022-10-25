9f211580c33307fceeb144fedaa09afcf8603fae
Michael Raiwet <michaelraiwet@icloud.com>
2022-10-25 01:21:24 +0000

    Testing Google Anayltics

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,10 @@ btn.addEventListener('click', async () => {
 		
 	}
 });
+
+// Google tag (gtag.js)
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-LV7R019EMV');
